f45d4dda2fb7d06bdc9c36bd203c79f1eea1ad31
Roberto Rivera <rzrivera@protonmail.com>
2022-02-27 21:21:21 -0800

    Refactor folders

--- tests/extension-tester.js
+++ tests/extension-tester.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const format = require('../vscode-extension/sql-transformer');
+const format = require('../src/sql-transformer');
 
 const loadedSql = fs.readFileSync(path.resolve(__dirname, 'sample.sql')).toString();
 

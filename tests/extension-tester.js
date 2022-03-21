9cc28010fc6f0e074986761a332fc2962d83d463
Roberto Rivera <rzrivera@protonmail.com>
2022-03-20 17:43:06 -0700

    Update tester with new settings parameter

--- tests/extension-tester.js
+++ tests/extension-tester.js
@@ -3,5 +3,11 @@ const path = require('path');
 const format = require('../src/sql-transformer');
 
 const loadedSql = fs.readFileSync(path.resolve(__dirname, 'sample.sql')).toString();
+const options = {
+  "blockComments": true,
+  "outcomeComments": true,
+  "startingWidth": 5,
+  "whereOneOne": true
+}
 
-console.log(format(loadedSql));
+console.log(format(loadedSql, options));
